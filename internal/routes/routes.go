8482142607ca31c6675b6d2ac0c981a70700b88f
Jean Ruiz <jeancarlosruizv@gmail.com>
2026-01-01 16:28:42 -0400

    validator for update bookmark

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -27,7 +27,7 @@ func Setup(router *gin.Engine) {
 		protectedGroup.POST("/bookmark", middleware.Validator[validator.CreateBookmark](), bookmarkCtrl.CreateBookmark)
 		protectedGroup.GET("/bookmark/user/:user_id", bookmarkCtrl.GetBookmarkByUserID)
 		protectedGroup.GET("/bookmark/user/:user_id/archived", bookmarkCtrl.GetArchivedBookmarkByUserID)
-		protectedGroup.PUT("/bookmark/update/:id", bookmarkCtrl.UpdateBookmark)
+		protectedGroup.PUT("/bookmark/update/:id", middleware.Validator[validator.UpdateBookmark](), bookmarkCtrl.UpdateBookmark)
 		protectedGroup.PUT("/bookmark/view-count/:id", bookmarkCtrl.IncrementVisitCountController)
 		protectedGroup.PUT("/bookmark/:id/toggle-pinned", bookmarkCtrl.TogglePinnedByIDController)
 		protectedGroup.PUT("/bookmark/:id/toggle-is-archived", bookmarkCtrl.ToggleIsArchiveByIDController)
